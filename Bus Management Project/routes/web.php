6544eb1271ebe955571ca2a37c00326f392a86a1
KARIMYASSe <karimyasser650@gmail.com>
2025-04-05 22:34:06 +0200

    Update web.php

--- Bus Management Project/routes/web.php
+++ Bus Management Project/routes/web.php
@@ -15,29 +15,12 @@ Route::prefix('admin')->middleware('admin')->group(function () {
     Route::resource('bookings', AdminBookingController::class);
 });
 
-// // لعرض نموذج تسجيل الدخول
-// Route::get('/admin/login', function () {
-//     return view('admin.login');
-// })->name('admin.login');
-
-// // مسار تسجيل الدخول (إذا لم يكن موجودًا بالفعل)
-// Route::post('/signin', [UserController::class, 'signin'])->name('signin');
-
-
-// use App\Http\Controllers\AdminController; // تأكد من المسار الصحيح
-// Route::get('/admin/welcome', function () {
-//     $user = session('user');
-//     return view('admin.welcome', compact('user'));
-// })->name('admin.welcome');
-
-
 Route::get('/login', function () {
     return view('admin.login');
 })->name('login');
 
 Route::post('/signin', [UserController::class, 'signin'])->name('signin');
 
-// صفحات الأدمن بعد تسجيل الدخول
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin/welcome', function () {
         return view('admin.welcome');
@@ -46,7 +29,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
 
     Route::get('/admin/panel', function () {
-        return view('admin.panel'); // دي ممكن تكون صفحة تدير منها الرحلات والحجوزات
+        return view('admin.panel'); 
     })->name('admin.panel');
 
     Route::get('/user/home', function () {
@@ -54,17 +37,6 @@ Route::middleware(['auth'])->group(function () {
     })->name('user.home');
 });
 
-
-// Route::get('/admin/bookings', function () {
-//     return view('admin.bookings');
-// })->name('admin.bookings')->middleware('auth');
-
-// Route::get('/admin/bookings', function () {
-//     if (Auth::user()->role !== 'admin') {
-//         abort(403, 'Unauthorized');
-//     }
-//     return view('admin.bookings');
-// })->name('admin.bookings')->middleware('auth');
 Route::get('/admin/bookings', function () {
     return view('admin.bookings');
 })->name('admin.bookings');
